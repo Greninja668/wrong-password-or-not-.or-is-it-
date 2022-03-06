6a41b3dc5a79e025f8f3de6339097f332a8bbd4c
Greninja668 <79901882+Greninja668@users.noreply.github.com>
2022-03-06 16:47:12 +0530

    Add files via upload

---
+++ dining.js
@@ -0,0 +1,41 @@
+img=""
+status1=""
+objects=[]
+function preload(){
+    img=loadImage("dinner.jpeg")
+}
+function setup(){
+    canvas=createCanvas(640,420)
+    canvas.center()
+    objectDetector=ml5.objectDetector("cocossd",modelLoaded)
+    document.getElementById("status").innerHTML="status:detecting objects"
+}
+function modelLoaded(){
+    console.log("modelLoaded")
+    status1=true
+    objectDetector.detect(img,gotResult)
+}
+function draw(){
+    image(img,0,0,640,420)
+    if (status1!="") {
+     for (let index = 0; index < objects.length; index++) {
+     document.getElementById("status").innerHTML="status: objects detected"
+     fill("#FF0000")
+     percent=floor(objects[index].confidence*100)
+    text(objects[index].label+" "+percent+"%",objects[index].x+15,objects[index].y+15)
+    noFill()
+    stroke("#FF0000")
+    rect(objects[index].x,objects[index].y,objects[index].width,objects[index].height)    
+     }
+    }
+}
+function gotResult(error,results){
+if (error) {
+  console.log(error)  
+}
+console.log(results)
+objects=results
+}
+function back(){
+    window.location="index.html"
+}
---
+++ main.js
@@ -0,0 +1,15 @@
+function kitchen(){
+window.location="kitchen.html"
+}
+function bedroom(){
+window.location="bedroom.html"    
+}
+function Dining_room(){
+window.location="dining.html"    
+}
+function Television(){
+window.location="television.html"    
+}
+function laptop_or_phone(){
+window.location="laptop.html"    
+}
